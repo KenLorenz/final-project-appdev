779b1f687561f433317c0d24b7ec53122e8a1145
KenLorenz <kenlorenz420@gmail.com>
2023-12-17 18:20:29 +0800

    fixed url

--- RentACar/RentACar/urls.py
+++ RentACar/RentACar/urls.py
@@ -15,34 +15,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from BootStrapAdmin.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('customer_view/', CustomerView.as_view(), name='customer-view'),
+    path('', IndexView, name='index'),
+    
+    path('customer_view', CustomerView.as_view(), name='customer-view'),
     path('customer_view/add', CustomerCreate.as_view(), name='customer-add'),
     path('customer_view/<pk>', CustomerUpdate.as_view(), name='customer-update'),
     path('customer_view/<pk>/delete', CustomerDelete.as_view(), name='customer-delete'),
     
-    path('employee_view/', EmployeeView.as_view(), name='employee-view'),
+    path('employee_view', EmployeeView.as_view(), name='employee-view'),
     path('employee_view/add', EmployeeCreate.as_view(), name='employee-add'),
     path('employee_view/<pk>', EmployeeUpdate.as_view(), name='employee-update'),
     path('employee_view/<pk>/delete', EmployeeDelete.as_view(), name='employee-delete'),
     
-    path('vehicle_view/', VehicleView.as_view(), name='vehicle-view'),
+    path('vehicle_view', VehicleView.as_view(), name='vehicle-view'),
     path('vehicle_view/add', VehicleCreate.as_view(), name='vehicle-add'),
     path('vehicle_view/<pk>', VehicleUpdate.as_view(), name='vehicle-update'),
     path('vehicle_view/<pk>/delete', VehicleDelete.as_view(), name='vehicle-delete'),
     
-    path('model_view/', ModelView.as_view(), name='model-view'),
+    path('model_view', ModelView.as_view(), name='model-view'),
     path('model_view/add', ModelCreate.as_view(), name='model-add'),
     path('model_view/<pk>', ModelUpdate.as_view(), name='model-update'),
     path('model_view/<pk>/delete', ModelDelete.as_view(), name='model-delete'),
     
-    path('rent_view/', RentView.as_view(), name='rent-view'),
+    path('rent_view', RentView.as_view(), name='rent-view'),
     path('rent_view/add', RentCreate.as_view(), name='rent-add'),
     path('rent_view/<pk>', RentUpdate.as_view(), name='rent-update'),
     path('rent_view/<pk>/delete', RentDelete.as_view(), name='rent-delete'),
